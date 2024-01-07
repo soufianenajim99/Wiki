f6c8c74f9d5074b5e94317b4691bc2cbb3e3ca5a
SoufianeNajim <soufianenajim100@gmail.com>
2024-01-07 13:43:10 +0100

    MVC structure

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<!-- <?php
+  require_once '../app/bootstrap.php';
+
+  echo'public';
+
+  // Init Core Library
+  $init = new Core;
+  ?> -->
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./src/output.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline ">
+        Hello world!
+    </h1>
+</body>
+
+</html>
---
+++ public/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../app/views/*.{php,html}", "./*.{php,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
